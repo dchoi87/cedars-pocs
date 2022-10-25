deefc7c5e80208d90c15b6ca34fe1ed6ef3ccbc6
Daniel Choi <dchoi87@gmail.com>
2022-10-25 16:44:04 -0700

    Search Animation Attempt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 window.addEventListener("DOMContentLoaded", function () {
-  var searchContainer = document.querySelector(".search");
+  var container = document.querySelector(".search");
 
   document.addEventListener("click", function (event) {
-    var searchBtn = this.querySelector(".main__search button");
+    var button = this.querySelector(".main__search button");
+    var input = this.querySelector(".yxt-SearchBar-input");
+    var main = this.querySelector(".main__container");
 
-    if (searchBtn.contains(event.target)) {
-      // searchContainer.style.display = "block";
-      searchContainer.classList.add("expanded");
+    if (button.contains(event.target) || input.contains(event.target)) {
+      container.classList.add("expanded");
       document.querySelector(".yxt-SearchBar-input").focus();
+
+      main.style.display = 'none';
     } else {
-      // searchContainer.style.display = "none";
-      searchContainer.classList.remove("expanded");
+      container.classList.remove("expanded");
+      main.style.display = '';
     }
   });
 });
