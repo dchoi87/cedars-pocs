a5a3570f3c4414956a5050bb5ab4bf3d7fc6e428
Daniel Choi <dchoi87@gmail.com>
2022-11-01 13:04:22 -0700

    Search Click

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 window.addEventListener("DOMContentLoaded", function () {
   var container = document.querySelector(".search");
-  var button = document.querySelector(".main__search button");
+  var desktopSearchBtn = document.querySelector(".main__search-btn");
+  var mobileSearchBtn = document.querySelector(".utility__search-btn");
+  var mobileMenuBtn = document.querySelector(".utility__menu-btn");
 
-  button.addEventListener("click", function (event) {
+  // desktop search
+  desktopSearchBtn.addEventListener("click", function () {
     container.classList.add("expanded");
     document.querySelector(".yxt-SearchBar-input").focus();
   });
-
+  // mobile search
+  mobileSearchBtn.addEventListener("click", function () {
+    container.classList.add("expanded");
+    document.querySelector(".yxt-SearchBar-input").focus();
+  });
+  // exit search on document click
   document.addEventListener("click", function (event) {
-    if (!event.target.closest(".main__search button, .yxt-SearchBar-input, .yxt-SearchBar-autocomplete")) {
-      // guard?
-      container.classList.remove("expanded");
+    if (!event.target.closest(".main__search-btn, .utility__search-btn, .yxt-SearchBar-input, .yxt-SearchBar-autocomplete")) {
+      if (container.matches(".expanded")) {
+        container.classList.remove("expanded");
+      }
     }
   });
-  
+  // exit search on esc
   document.addEventListener("keydown", function(event) {
-    const key = event.key;
+    var key = event.key;
     if (key === "Escape") {
       container.classList.remove("expanded");
     }
   });
+  // mobile menu
+  mobileMenuBtn.addEventListener("click", function (event) {
+    console.log(event.currentTarget)
+  });
+
 });
