0250cc7fc6919d2bb1004367f05ad334f7a168cf
Daniel Choi <dchoi87@gmail.com>
2022-11-02 13:20:09 -0700

    Mobile Menu

--- src/index.js
+++ src/index.js
@@ -11,14 +11,23 @@ window.addEventListener("DOMContentLoaded", function () {
   });
   // mobile search
   mobileSearchBtn.addEventListener("click", function () {
-    container.classList.add("expanded");
-    document.querySelector(".yxt-SearchBar-input").focus();
+    if (container.matches(".expanded")) {
+      container.classList.remove("expanded");
+      this.classList.remove("active");
+      mobileMenuBtn.classList.remove("disabled");
+    } else {
+      container.classList.add("expanded");
+      document.querySelector(".yxt-SearchBar-input").focus();
+      this.classList.add("active");
+      mobileMenuBtn.classList.add("disabled");
+    }
   });
   // exit search on document click
   document.addEventListener("click", function (event) {
     if (!event.target.closest(".main__search-btn, .utility__search-btn, .yxt-SearchBar-input, .yxt-SearchBar-autocomplete")) {
       if (container.matches(".expanded")) {
         container.classList.remove("expanded");
+        mobileSearchBtn.classList.remove("active");
       }
     }
   });
